6fe954b78ac0d4bf0241b01cff762cf220395757
wub <wubin8878@dingtalk.com>
2020-01-16 17:52:06 +0800

    Initial commit

---
+++ nacos-produce/src/main/java/com/springcloud/demo/NacosProduceApplication.java
@@ -0,0 +1,16 @@
+package com.springcloud.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosProduceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosProduceApplication.class, args);
+    }
+
+}
